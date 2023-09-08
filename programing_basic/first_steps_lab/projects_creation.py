c51c1d0ba26ccd48284b4728e2ef8f5a0ecf9545
Petar-Dinev <p.dinev55@gmail.com>
2023-09-08 22:53:50 +0300

    Add one task from Python programing basics

---
+++ programing_basic/first_steps_lab/projects_creation.py
@@ -0,0 +1,5 @@
+name_of_architect = input()
+numbers_of_projects = int(input())
+time_for_all_projects = numbers_of_projects * 3
+
+print(f"The architect { name_of_architect } will need {time_for_all_projects} hours to complete {numbers_of_projects} project/s.")
