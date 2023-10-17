d21a07ccf4689ae8cb42f74ec9c8a1af6597888e
Petar-Dinev <p.dinev55@gmail.com>
2023-10-17 22:06:47 +0300

    Add one task from Python Programing Basic

---
+++ programing_basic/Conditional Statements Advanced - Lab/fruit_or_vegetable.py
@@ -0,0 +1,8 @@
+product = input()
+
+if product == 'banana' or product == 'kiwi' or product == 'lemon' or product == 'grapes' or product == 'apple' or product == 'cherry':
+    print('fruit')
+elif product == 'tomato' or product == 'carrot' or product == 'cucumber' or product == 'pepper':
+    print('vegetable')
+else:
+    print('unknown')
