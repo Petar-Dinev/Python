fed2178766c5930e7bbe3042610f8df2b5776c2e
Petar-Dinev <p.dinev55@gmail.com>
2024-01-09 20:46:07 +0200

    Add one task from Python Fundamentals module

---
+++ fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/number_definer.py
@@ -0,0 +1,20 @@
+num = float(input())
+
+if num == 0:
+    print("zero")
+elif num > 0:
+    if abs(num) < 1:
+        print("small positive")
+    elif abs(num) > 1000000:
+        print("large positive")
+    else:
+        print("positive")
+else:
+    if abs(num) < 1:
+        print("small negative")
+    elif abs(num) > 1000000:
+        print("large negative")
+    else:
+        print("negative")
+
+
