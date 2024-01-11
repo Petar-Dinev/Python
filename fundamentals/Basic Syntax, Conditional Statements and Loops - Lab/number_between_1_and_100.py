a3563724460fbf57243c53c3eea4bb0d55bc7330
Petar-Dinev <p.dinev55@gmail.com>
2024-01-11 21:58:59 +0200

    Add task from Python Fundamentals module

---
+++ fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/number_between_1_and_100.py
@@ -0,0 +1,13 @@
+# while True:
+#     num = float(input())
+#
+#     if 1 <= num <= 100:
+#         print(f"The number {num} is between 1 and 100")
+#         break
+
+num = float(input())
+
+while num < 1 or num > 100:
+    num = float(input())
+
+print(f"The number {num} is between 1 and 100")
