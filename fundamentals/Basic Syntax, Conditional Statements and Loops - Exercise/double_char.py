2562e9a6cf3fbe365ca49383176256ef59a130e1
Petar-Dinev <p.dinev55@gmail.com>
2024-01-13 16:38:13 +0200

    Add task from Python Fundamentals module

---
+++ fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/double_char.py
@@ -0,0 +1,9 @@
+string = input()
+
+while string != 'End':
+    if string != 'SoftUni':
+        for char in string:
+            print(char, end="")
+            print(char, end="")
+        print()
+    string = input()
