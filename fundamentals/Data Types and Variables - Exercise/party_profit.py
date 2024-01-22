ea79b222716b79f7d5241eec4e31fc271522d415
Petar-Dinev <p.dinev55@gmail.com>
2024-01-22 23:11:17 +0200

    Add one task from Python Fundamentals module

---
+++ fundamentals/Data Types and Variables - Exercise/party_profit.py
@@ -0,0 +1,18 @@
+group_size = int(input())
+days = int(input())
+coins = 0
+
+for i in range(1, days + 1):
+    if i % 10 == 0:
+        group_size -= 2
+    if i % 15 == 0:
+        group_size += 5
+    coins += (50 - group_size * 2)
+    if i % 3 == 0:
+        coins -= group_size * 3
+    if i % 5 == 0:
+        coins += group_size * 20
+        if i % 3 == 0:
+            coins -= group_size * 2
+
+print(f"{group_size} companions received {coins // group_size} coins each.")
