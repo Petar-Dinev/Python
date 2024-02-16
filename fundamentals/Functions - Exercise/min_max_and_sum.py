f6f1829178be606122ca619190b74df997c1483c
Petar-Dinev <p.dinev55@gmail.com>
2024-02-16 23:29:16 +0200

    Add one task from Python Fundamentals

---
+++ fundamentals/Functions - Exercise/min_max_and_sum.py
@@ -0,0 +1,9 @@
+nums_list = [int(num) for num in input().split()]
+
+min_num = min(nums_list)
+max_num = max(nums_list)
+sum_of_nums = sum(nums_list)
+
+print(f'The minimum number is {min_num}')
+print(f'The maximum number is {max_num}')
+print(f'The sum number is: {sum_of_nums}')
