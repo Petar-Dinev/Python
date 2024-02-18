9d509ec011d7deb84ee2589319f84d50370c41a7
Petar-Dinev <p.dinev55@gmail.com>
2024-02-18 22:07:59 +0200

    Add task from Python Fundamentals module

---
+++ fundamentals/Functions - Exercise/smallest_of_three_numbers.py
@@ -0,0 +1,10 @@
+def return_smallest_from_three_nums(num1, num2, num3):
+    return min(num1, num2, num3)
+
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+
+
+print(return_smallest_from_three_nums(first_num, second_num, third_num))
