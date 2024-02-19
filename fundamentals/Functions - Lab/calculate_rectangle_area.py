cea85b9f4e1a67b7c95a3353bf0e976342702775
Petar-Dinev <p.dinev55@gmail.com>
2024-02-19 23:11:22 +0200

    Add one task from Python Fundamentals

---
+++ fundamentals/Functions - Lab/calculate_rectangle_area.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_area(width, height):
+    print(height * width)
+
+
+input_width = int(input())
+input_height = int(input())
+calculate_rectangle_area(input_width, input_height)
