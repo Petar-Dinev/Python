f7805cc2757e1aeeb8f2665588f709b9fc4e0702
Petar-Dinev <p.dinev55@gmail.com>
2024-02-28 11:10:51 +0200

    Add task from Python Fundamentals

---
+++ fundamentals/Objects and Classes - Exericse/catalogue_.py
@@ -0,0 +1,17 @@
+class Catalogue:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product_name: str):
+        self.products.append(product_name)
+
+    def get_by_letter(self, first_letter: str):
+        return [product for product in self.products if product.startswith(first_letter)]
+
+    def __repr__(self):
+        returned_string = f"Items in the {self.name} catalogue:\n"
+        returned_string += '\n'.join(sorted(self.products))
+        return returned_string
+
