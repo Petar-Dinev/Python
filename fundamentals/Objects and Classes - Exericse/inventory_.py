0eb8f9c65a83cbf443d044c956fb22fecd8676b9
Petar-Dinev <p.dinev55@gmail.com>
2024-03-16 23:51:45 +0200

    rework task from Python Fundamentals

--- fundamentals/Objects and Classes - Exericse/inventory_.py
+++ fundamentals/Objects and Classes - Exericse/inventory_.py
@@ -1,13 +1,12 @@
 class Inventory:
 
-    def __init__(self, capacity: int):
-        self.__capacity = capacity
+    def __init__(self, __capacity: int):
+        self.__capacity = __capacity
         self.items = []
 
     def add_item(self, item: str):
-        if self.__capacity > 0:
+        if self.__capacity > len(self.items):
             self.items.append(item)
-            self.__capacity -= 1
         else:
             return "not enough room in the inventory"
 
@@ -16,5 +15,5 @@ class Inventory:
 
     def __repr__(self):
         items = ', '.join(self.items)
-        left_capacity = self.__capacity
+        left_capacity = self.__capacity - len(self.items)
         return f"Items: {items}.\nCapacity left: {left_capacity}"
