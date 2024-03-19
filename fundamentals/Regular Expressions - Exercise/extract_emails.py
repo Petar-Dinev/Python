b58dd03c4801f91ea1eb291cfd3db78968169d83
Petar-Dinev <p.dinev55@gmail.com>
2024-03-19 23:37:28 +0200

    Add task from Python Fundamentals module

---
+++ fundamentals/Regular Expressions - Exercise/extract_emails.py
@@ -0,0 +1,8 @@
+import re
+
+sentence = input()
+pattern = r'\s(([a-z0-9]+)([\.\_\-]*[a-z0-9])*@([a-z]+(\-*[a-z]+)*)(\.([a-z]+\-*[a-z]+))+)\b'
+
+matches = re.findall(pattern, sentence)
+for match in matches:
+    print(match[0])
