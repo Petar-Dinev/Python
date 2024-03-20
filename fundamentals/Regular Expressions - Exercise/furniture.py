ed007a6eb0ee7234786a084756d8f56049f42d8a
Petar-Dinev <p.dinev55@gmail.com>
2024-03-20 23:30:25 +0200

    Add one task from Python Fundamentals module

---
+++ fundamentals/Regular Expressions - Exercise/furniture.py
@@ -0,0 +1,19 @@
+import re
+
+bought_furniture = []
+total_cost = 0
+pattern = r'>>([A-Za-z]+)<<(\d+.?\d+)!(\d+)'
+line = input()
+
+while line != 'Purchase':
+    match = re.search(pattern, line)
+    if match:
+        furniture, price, quantity = match.groups()
+        bought_furniture.append(furniture)
+        total_cost += float(price) * int(quantity)
+
+    line = input()
+
+print("Bought furniture:")
+print('\n'.join(bought_furniture))
+print(f"Total money spend: {total_cost:.2f}")
