431e744f3b78274545abc719b3e5498dc3ca946a
Petar-Dinev <p.dinev55@gmail.com>
2024-07-12 17:45:26 +0300

    Add solution of task from Python OOP module

---
+++ python_oop/Inheritance - Exercise/04_need_for_speed/project/car.py
@@ -0,0 +1,6 @@
+from project.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    DEFAULT_FUEL_CONSUMPTION: float = 3
+    pass
---
+++ python_oop/Inheritance - Exercise/04_need_for_speed/project/vehicle.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION: float = 1.25
+
+    def __init__(self, fuel: float, horse_power: int):
+        self.fuel = fuel
+        self.horse_power = horse_power
+        self.fuel_consumption: float = self.DEFAULT_FUEL_CONSUMPTION
+
+    def drive(self, kilometers: float):
+        consumption = kilometers * self.fuel_consumption
+        if consumption <= self.fuel:
+            self.fuel -= consumption
