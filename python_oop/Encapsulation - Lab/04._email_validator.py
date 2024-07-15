834dc321519b10c7f04d5305d19be9191db2a562
Petar-Dinev <p.dinev55@gmail.com>
2024-07-15 17:46:56 +0300

    Add solution of task from Python OOP module

---
+++ python_oop/Encapsulation - Lab/04._email_validator.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class EmailValidator:
+    def __init__(self, min_length: int, mails: List[str], domains: List[str]):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name: str) -> bool:
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail: str) -> bool:
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain: str) -> bool:
+        return domain in self.domains
+
+    def validate(self, email):
+        name, mail_data = email.split('@')
+        mail, domain = mail_data.split('.')
+        return self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)
