4029612c4f29f897e23ae3f55263c369a92f8062
Petar-Dinev <p.dinev55@gmail.com>
2024-07-15 17:47:01 +0300

    Add solution of task from Python OOP module

---
+++ python_oop/Encapsulation - Lab/05._account.py
@@ -0,0 +1,19 @@
+class Account:
+    def __init__(self, id: int, balance: int, pin: int):
+        self.__id = id
+        self.balance = balance
+        self.__pin = pin
+
+    def get_id(self, pin: int):
+
+        if pin == self.__pin:
+            return self.__id
+
+        return "Wrong pin"
+
+    def change_pin(self, old_pin: int, new_pin: int) -> str:
+        if old_pin == self.__pin:
+            self.__pin = new_pin
+            return "Pin changed"
+
+        return "Wrong pin"
