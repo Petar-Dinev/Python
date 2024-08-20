5584911e7f57643af8048e846c83016b662d21a0
Petar-Dinev <p.dinev55@gmail.com>
2024-08-20 10:04:04 +0300

    Add one task from Python DB Module

---
+++ python_db/postgre_sql/Data Types and Table Basics - Lab/insert_data_in_tables.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees
+VALUES ('1', 'Petar', 'Dinev', '2023-01-02', '2500', '123');
+INSERT INTO employees
+VALUES ('2', 'Anita', 'Dineva', '2023-01-03', '1500', '124');
+INSERT INTO employees
+VALUES ('3', 'Bojidar', 'Dinev', '2023-01-04', '1200', '125');
