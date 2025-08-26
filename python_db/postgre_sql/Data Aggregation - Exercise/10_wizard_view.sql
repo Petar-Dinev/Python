59b660b67d96718dbabccf2a02d990b1bda2b39f
Petar Dinev <p.dinev55@gmail.com>
2025-08-26 09:30:08 +0300

    Add one solution of task from Python DB module

---
+++ python_db/postgre_sql/Data Aggregation - Exercise/10_wizard_view.sql
@@ -0,0 +1,12 @@
+CREATE VIEW view_wizard_deposits_with_expiration_date_before_1983_08_17
+	AS
+SELECT
+	CONCAT(first_name, ' ', last_name) AS wizard_name,
+	deposit_start_date AS start_date,
+	deposit_expiration_date AS expiration_date,
+	deposit_amount AS amount
+FROM
+	wizard_deposits
+GROUP BY wizard_name, deposit_start_date, deposit_expiration_date, deposit_amount
+HAVING deposit_expiration_date <= '1983-08-17'
+ORDER BY expiration_date ASC;
