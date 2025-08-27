bbae3a252a5ff117a97c51c57812366f262d33b3
Petar Dinev <p.dinev55@gmail.com>
2025-08-27 09:31:17 +0300

    Add one solution of task from Python DB module

---
+++ python_db/postgre_sql/Data Aggregation - Exercise/12_age_group.sql
@@ -0,0 +1,17 @@
+SELECT
+	CASE
+		WHEN age < 11 THEN '[0-10]'
+		WHEN age < 21 THEN '[11-20]'
+		WHEN age < 31 THEN '[21-30]'
+		WHEN age < 41 THEN '[31-40]'
+		WHEN age < 51 THEN '[41-50]'
+		WHEN age < 61 THEN '[51-60]'
+		ELSE '[61+]'
+	END AS age_group,
+	COUNT(*)
+FROM wizard_deposits
+GROUP BY
+    age_group
+ORDER BY
+    age_group ASC
+;
