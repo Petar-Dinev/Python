0b3738751654c527ec8ea3ed30818092a096eb38
Petar Dinev <p.dinev55@gmail.com>
2025-08-28 09:32:24 +0300

    Add one solution of task from Python DB module

---
+++ python_db/postgre_sql/Data Aggregation - Exercise/17_having_salary_level.sql
@@ -0,0 +1,12 @@
+SELECT
+	department_id,
+	COUNT(*),
+	CASE
+		WHEN AVG(salary) > 50000 THEN 'Above average'
+		WHEN AVG(salary) <= 50000 THEN 'Below average'
+	END AS salary_level
+FROM
+	employees
+GROUP BY department_id
+HAVING AVG(salary) > 30000
+ORDER BY department_id
