8206c4305940b7ef8cf30d24d8f8a2d0f47276d6
Petar Dinev <p.dinev55@gmail.com>
2025-09-07 10:53:29 +0300

    Add one task from Python DB module

---
+++ python_db/postgre_sql/Database Programmability - Lab/04_triggered.sql
@@ -0,0 +1,27 @@
+CREATE TABLE deleted_employees(
+	employee_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	middle_name VARCHAR(20),
+	job_title VARCHAR(50),
+	department_id INT,
+	salary NUMERIC(19,4)
+);
+
+CREATE OR REPLACE FUNCTION fn_collect_deleted_employees()
+RETURNS TRIGGER
+AS
+$$
+	BEGIN
+		INSERT INTO deleted_employees(first_name, last_name, middle_name, job_title, department_id, salary)
+		VALUES
+			(old.first_name, old.last_name, old.middle_name, old.job_title, old.department_id, old.salary);
+		RETURN OLD;
+	END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER collect_deleted_employees_trigger
+AFTER DELETE ON employees
+FOR EACH ROW EXECUTE PROCEDURE fn_collect_deleted_employees()
+
