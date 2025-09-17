c55ffce266710aac9deb9089c4dd4e10d79af769
Petar Dinev <p.dinev55@gmail.com>
2025-09-17 15:33:43 +0300

    Add solution of task from Python DB module

---
+++ python_db/postgre_sql/Subqueries and JOINs - Exercise/04_booking_information.sql
@@ -0,0 +1,18 @@
+SELECT
+	b.booking_id,
+	a.name AS apartment_owner,
+	a.apartment_id,
+	CONCAT_WS(' ', c.first_name, c.last_name) AS customer_name
+FROM
+	bookings AS b
+FULL JOIN
+	apartments AS a
+ON
+	b.booking_id = a.booking_id
+FULL JOIN
+	customers AS c
+ON
+	b.customer_id = c.customer_id
+ORDER BY
+	b.booking_id, apartment_owner, customer_name
+;
