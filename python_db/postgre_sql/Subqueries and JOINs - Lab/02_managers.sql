1af3ba3d69506f0dfa06e2a1a673dc34ea83ec8d
Petar Dinev <p.dinev55@gmail.com>
2025-09-23 14:12:58 +0300

    Add solution of task from Python DB module

---
+++ python_db/postgre_sql/Subqueries and JOINs - Lab/02_managers.sql
@@ -0,0 +1,14 @@
+SELECT
+	e.employee_id,
+	CONCAT(e.first_name, ' ', last_name) AS full_name,
+	d.department_id,
+	d.name AS department_name
+FROM
+	employees AS e
+JOIN
+	departments AS d
+ON
+	e.employee_id = d.manager_id
+ORDER BY
+	employee_id
+LIMIT 5;
